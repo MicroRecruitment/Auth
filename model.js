7cf320b16c57397d87a82e662b607610fa8c036b
Linus Berg <linusberg@n165-p162.eduroam.kth.se>
2020-03-02 10:50:09 +0100

    Formatting.

--- model.js
+++ model.js
@@ -18,12 +18,12 @@ class Model {
     return ret;
   }
   /*
-	* Register user.
-  * @queue: auth_queue
-	* @author: Linus Berg
-	* @param {obj} Registration data.
-	* @return {obj} Queue Reply Status.
-	*/
+   * Register user.
+   * @queue: auth_queue
+   * @author: Linus Berg
+   * @param {obj} Registration data.
+   * @return {obj} Queue Reply Status.
+   */
   async Register(registration_data) {
     registration_data.role_id = 0;
     let result = await db.AddUser(registration_data);
@@ -31,12 +31,12 @@ class Model {
   }
 
   /*
-	* Authenticate user.
-  * @queue: auth_queue
-	* @author: Linus Berg
-	* @param {obj} Login data.
-	* @return {obj} Queue Reply Status.
-	*/
+   * Authenticate user.
+   * @queue: auth_queue
+   * @author: Linus Berg
+   * @param {obj} Login data.
+   * @return {obj} Queue Reply Status.
+   */
   async Login(login_data) {
     let result = await db.GetUserWithPassword(login_data);
     result = await this.ProcessResult(result);
@@ -45,12 +45,12 @@ class Model {
   }
 
   /*
-	* Get user by username.
-  * @queue: auth_queue
-	* @author: Linus Berg
-	* @param {string} Username.
-	* @return {obj} Queue Reply Status.
-	*/
+   * Get user by username.
+   * @queue: auth_queue
+   * @author: Linus Berg
+   * @param {string} Username.
+   * @return {obj} Queue Reply Status.
+   */
   async GetUser(username) {
     let result = await db.GetUser(username);
     let ret = new Result(false, []);
